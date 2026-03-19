046d31746bbb84b4301041fda17fe9d3ba0855ba
Sowmya M R <sowmyamr@192.168.1.8>
2026-03-19 23:25:11 +0530

    Add validations to Employee model to pass tests

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ApplicationRecord
-  validates :full_name, :job_title, presence: true
+  validates :full_name, :job_title, :country, presence: true
 
   validates :salary, presence: true, numericality: { greater_than: 0 }
 end

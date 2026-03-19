1cb6fc583ce8ce427575010bde39cc96ff9f1728
Sowmya M R <sowmyamr@192.168.1.8>
2026-03-20 01:19:54 +0530

    adding indexes

--- db/migrate/20260319173220_create_employees.rb
+++ db/migrate/20260319173220_create_employees.rb
@@ -2,8 +2,8 @@ class CreateEmployees < ActiveRecord::Migration[8.1]
   def change
     create_table :employees do |t|
       t.string :full_name
-      t.string :job_title
-      t.string :country
+      t.string :job_title, index: true
+      t.string :country, index: true
       t.decimal :salary, precision: 10, scale: 2, null: false
 
       t.timestamps

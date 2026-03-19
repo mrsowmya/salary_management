2d6a072400327fa515b8ff351fe090c6ead4e0db
Sowmya M R <sowmyamr@192.168.1.8>
2026-03-19 23:30:39 +0530

    Refactor the Employee Spec

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -3,36 +3,48 @@ require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
 
+  subject do
+    described_class.new(
+      full_name: 'Sowmya',
+      job_title: 'Software Engineer',
+      country: 'India',
+      salary: 50000
+    )
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
   it "is invalid without full_name" do
-    employee = Employee.new(full_name: nil, salary: '1000000', job_title: 'Software Engineer', country: 'India')
-    expect(employee).not_to be_valid
+    subject.full_name = nil
+    expect(subject).not_to be_valid
   end
 
   it "is invalid without salary" do
-    employee = Employee.new(full_name: 'sowmya', salary: nil, job_title: 'Software Engineer', country: 'India')
-    expect(employee).not_to be_valid
+    subject.salary = nil
+    expect(subject).not_to be_valid
   end
 
   it "is invalid without Job Title" do
-    employee = Employee.new(full_name: 'sowmya', salary: '1000000', job_title: nil, country: 'India')
-    expect(employee).not_to be_valid
+    subject.job_title = nil
+    expect(subject).not_to be_valid
   end
 
   it "is invalid without Country" do
-    employee = Employee.new(full_name: 'sowmya', salary: '1000000', job_title: 'Software Engineer', country: nil)
-    expect(employee).not_to be_valid
+    subject.country = nil
+    expect(subject).not_to be_valid
   end
 
   it 'is expected that salary should be greater than 0' do
-    employee = Employee.new(full_name: 'sowmya', salary: '0', job_title: 'Software Engineer', country: 'India')
-
-    expect(employee).not_to be_valid
+    subject.salary = 0
+    expect(subject).not_to be_valid
   end
 
   it 'is expected that salary should be numeric' do
-    employee = Employee.new(full_name: 'sowmya', salary: 'abc', job_title: 'Software Engineer', country: 'India')
+    subject.salary = 'abc'
 
-    employee.valid?
-    expect(employee.errors[:salary]).to include("is not a number")
+    subject.valid?
+    expect(subject.errors[:salary]).to include("is not a number")
   end
 end

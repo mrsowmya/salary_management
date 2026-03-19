f24af6c9e3b4326672e560f1c478b87ac42989b5
Sowmya M R <sowmyamr@192.168.1.8>
2026-03-19 23:19:09 +0530

    adding more test cases Employee

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -17,5 +17,17 @@ RSpec.describe Employee, type: :model do
     employee = Employee.new(full_name: 'sowmya', salary: '1000000', job_title: nil)
     expect(employee).not_to be_valid
   end
- 
+
+  it 'is expected that salary should be greater than 0' do
+    employee = Employee.new(full_name: 'sowmya', salary: '0', job_title: 'Software Engineer')
+
+    expect(employee).not_to be_valid
+  end
+
+  it 'is expected that salary should be numeric' do
+    employee = Employee.new(full_name: 'sowmya', salary: 'abc', job_title: 'Software Engineer')
+
+    employee.valid?
+    expect(employee.errors[:salary]).to include("is not a number")
+  end
 end

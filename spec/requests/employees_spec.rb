10cea3ceb18d893607d55604b9d5ac73963b7985
Sowmya M R <sowmyamr@192.168.1.8>
2026-03-19 23:45:19 +0530

    Adding failure test case Employee show'

--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Employees API", type: :request do
 
   let!(:employees) do
-    3.times do
+    3.times.map do
       Employee.create!(
         full_name: 'Test User',
         job_title: 'Software Engineer',
@@ -19,6 +19,15 @@ RSpec.describe "Employees API", type: :request do
       get '/employees'
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body).size).to eq(3)
+    end
+  end
+
+  describe 'GET /employees/:id' do
+    it 'returns employee' do
+
+      get "/employees/#{employees.first.id}"
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to eq(employees.first)
     end
   end
 

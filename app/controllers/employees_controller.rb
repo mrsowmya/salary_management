982a301eb897388b7413a960ac0adb89af8232a9
Sowmya M R <sowmyamr@192.168.1.8>
2026-03-19 23:50:37 +0530

    Adding passing test cases Employee show

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,8 +1,22 @@
 class EmployeesController < ApplicationController
 
+  before_action :set_employee, only: [:show]
+
   def index
     @employees = Employee.all
 
     render json: @employees
   end
+
+  def show
+    render json: @employee
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find_by(id: params[:id])
+  end
+
+
 end

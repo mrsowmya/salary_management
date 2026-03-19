275740e21cad46079d9f3e172e9c4eddd769015d
Sowmya M R <sowmyamr@192.168.1.8>
2026-03-19 23:12:00 +0530

    Employee Spec

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -2,5 +2,20 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
+
+  it "is invalid without full_name" do
+    employee = Employee.new(full_name: nil, salary: '1000000', job_title: 'Software Engineer')
+    expect(employee).not_to be_valid
+  end
+
+  it "is invalid without salary" do
+    employee = Employee.new(full_name: 'sowmya', salary: nil, job_title: 'Software Engineer')
+    expect(employee).not_to be_valid
+  end
+
+  it "is invalid without Job Title" do
+    employee = Employee.new(full_name: 'sowmya', salary: '1000000', job_title: nil)
+    expect(employee).not_to be_valid
+  end
  
 end
